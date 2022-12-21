21e45998571241d995d6d1b2416130f81d9fabfb
Pradip Teli <vptelipradip@gmail.com>
2022-12-21 14:34:43 +0530

    End of section16

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -27,7 +27,9 @@ namespace API.Controllers
         [HttpPost("login")]
         public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
         {
-            var user = await _userManager.FindByEmailAsync(loginDto.Email);
+           // var user = await _userManager.FindByEmailAsync(loginDto.Email);
+            var user = await _userManager.Users.Include(u=>u.Photos)
+                .FirstOrDefaultAsync(x=>x.Email==loginDto.Email);
             if (user == null) return Unauthorized();
 
             var result = await _userManager.CheckPasswordAsync(user, loginDto.Password);
@@ -74,7 +76,9 @@ namespace API.Controllers
         [Authorize]
         public async Task<ActionResult<UserDto>> GetCurrentUser()
         {
-            var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+            // var user = await _userManager.FindByEmailAsync(User.FindFirstValue(ClaimTypes.Email));
+            var user = await _userManager.Users.Include(u => u.Photos)
+                 .FirstOrDefaultAsync(x => x.Email == User.FindFirstValue(ClaimTypes.Email));
             return CreateUserObject(user);
         }
 
@@ -83,7 +87,7 @@ namespace API.Controllers
             return new UserDto
             {
                 DisplayName = user.DisplayName,
-                Image = null,
+                Image = user.Photos?.FirstOrDefault(x=>x.IsMain)?.Url,
                 Token = _tokenService.CrateToken(user),
                 UserName = user.UserName
             };
---
+++ API/Controllers/ProfilesController.cs
@@ -0,0 +1,17 @@
+﻿using Application.Profiles;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    
+    public class ProfilesController : BaseApiContoller
+    {
+        [HttpGet("{username}")]
+
+        public async Task<IActionResult> GetProfile(string username)
+        {
+            return HandleResult(await Mediator.Send(new Details.Query { Username = username }));
+        }
+    }
+}
